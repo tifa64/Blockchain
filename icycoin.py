89c57fb92706053297fd7f2b2009ca24a7edae28
Mostafa Ali Mansour <tifa64@users.noreply.github.com>
2019-12-22 21:04:03 +0100

    Adding transactions to the Blockchain

--- icycoin.py
+++ icycoin.py
@@ -130,6 +130,16 @@ def is_valid():
     response = {'message': blockchain.is_chain_valid(blockchain.chain)}
     return jsonify(response), 200
 
+# Adding a new transaction to the Blockchain
+@app.route('add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all (key in json for key in transaction_keys):
+        return 'Some elements of the transaction are missing', 400
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'This transaction will be added to Block {index}'}
+    return json(response), 201
 # Part 3 - Decentralizing our Blockchains
 
 # Running the app
